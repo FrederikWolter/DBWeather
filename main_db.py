8f9a08d9d23f426562ed1cb5e351857df0b75b67
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-10-28 17:59:59 +0200

    added mongoDB example

--- main_db.py
+++ main_db.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pymongo
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def main():
+    myclient = pymongo.MongoClient("mongodb://root:example@localhost:27017/")
+
+    mydb = myclient["mydatabase"]
+
+    print(myclient.list_database_names())
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
